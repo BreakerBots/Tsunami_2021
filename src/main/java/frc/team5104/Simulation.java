61760be8052b6e03c2ae88e10bb96b648ada75ad
Liam Snow <liamsnow03@gmail.com>
2021-01-23 22:26:51 -0500

    ...

--- src/main/java/frc/team5104/Simulation.java
+++ src/main/java/frc/team5104/Simulation.java
@@ -10,14 +10,5 @@ public class Simulation {
         Plotter.reset();
 
         (new CoordinateExample()).plot();
-
-//        while (true) {
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println("test");
-//        }
     }
 }
