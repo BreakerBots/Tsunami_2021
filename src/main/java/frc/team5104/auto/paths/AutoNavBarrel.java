caf3365b79cd79c369283dcd370bc8a13b2a14fc
JackWeyant <58121774+JackWeyant@users.noreply.github.com>
2021-02-13 16:32:50 -0800

    fine tuned the barrel path

--- src/main/java/frc/team5104/auto/paths/AutoNavBarrel.java
+++ src/main/java/frc/team5104/auto/paths/AutoNavBarrel.java
@@ -13,27 +13,28 @@ public class AutoNavBarrel extends AutoPath {
             // Evading Marker D5
             run(new DriveTrajectoryAction(false,
                     new Position(5, 7.5, 0), // Start
-                    new Position(9.1, 9.1, 0), 
-                    new Position(15, 3.4, -90),
-                    new Position(10.9, 2.9, -180),
-                    new Position(9.2, 6.6, 90)    
+                    new Position(9.1, 7.5, 0),
+                    new Position(15, 5, -90),
+                    new Position(10.9, 2.9, -220),
+                    new Position(9.2, 8.2, 70)
             ));
             // Evading Marker B8
             run(new DriveTrajectoryAction(false, 
-                    new Position(9.2, 6.6, 90), // Last point
-                    new Position(23.9, 8.7, 30),
-                    new Position(18.4, 13.2, -180),
-                    new Position(17, 8.5, 90)
+                    new Position(9.2, 8.2, 70), // Last point
+                    new Position(21.9, 7.6, 0),
+                    new Position(23.6, 11.0, 110),
+                    new Position(18.4, 13.2, 180),
+                    new Position(17, 8.5, 290)
             ));
             // Evading Marker D10
             run(new DriveTrajectoryAction(false, 
-                    new Position(17, 8.5, 90), // Last point
-                    new Position(20.3, 3.9, 45),
+                    new Position(17, 8.5, 290), // Last point
+                    new Position(20.3, 3.9, 325),
                     new Position(26.5, 2.6, 0),
-                    new Position(28, 6.9, 90),
+                    new Position(28, 5.7, 120),
                     new Position(22, 7.35, 195),
                     new Position(15.4, 8, 165),
-                    new Position(7.5, 0, 180) // Finish
+                    new Position(0, 7.5, 180) // Finish
             ));
         }
     }
