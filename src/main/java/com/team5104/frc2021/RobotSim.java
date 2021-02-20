ff86c0bba28d8b239cd056a960443f74d00825bb
Liam Snow <liamsnow03@gmail.com>
2021-02-20 05:36:08 -0500

    Update RobotSim.java

--- src/main/java/com/team5104/frc2021/RobotSim.java
+++ src/main/java/com/team5104/frc2021/RobotSim.java
@@ -1,34 +1,33 @@
 package com.team5104.frc2021;
 
 import com.team5104.frc2021.auto.paths.AutoNavSlalom;
-import com.team5104.frc2021.subsystems.Drive;
+import com.team5104.frc2021.subsystems.*;
 import com.team5104.lib.auto.AutoManager;
 import com.team5104.lib.auto.Odometry;
 import com.team5104.lib.dashboard.Dashboard;
-import com.team5104.lib.managers.SubsystemManager;
 import com.team5104.lib.setup.RobotController;
+import com.team5104.lib.subsystem.SubsystemManager;
 import com.team5104.lib.webapp.Plotter;
 import com.team5104.lib.webapp.Webapp;
 
 public class RobotSim extends RobotController.BreakerRobot {
     public RobotSim() {
         //Win
-        this.win();
+        win();
 
         //Managers
-        SubsystemManager.useSubsystems(
+        SubsystemManager.attach(
             new Drive()
         );
 
         //Other Initialization
-        //Dashboard.run();
+        //Dashboard.init();
         Webapp.run();
         Plotter.reset();
         Odometry.init();
         AutoManager.setTargetPath(new AutoNavSlalom());
         //AutoManager.runTrajectoryTester();
         AutoManager.enabledPlotting();
-        AutoManager.characterize(Drive.class);
     }
 
     //Autonomous
