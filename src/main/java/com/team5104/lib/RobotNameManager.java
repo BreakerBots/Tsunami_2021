eb85a545044f08b0cf78f57bca48ade937ad6e62
Liam Snow <liamsnow03@gmail.com>
2021-02-26 20:03:12 -0500

    Update RobotNameManager.java

--- src/main/java/com/team5104/lib/RobotNameManager.java
+++ src/main/java/com/team5104/lib/RobotNameManager.java
@@ -1,9 +1,5 @@
 package com.team5104.lib;
 
-import com.team5104.lib.setup.RobotState;
-
-import java.util.Arrays;
-
 //Robot Name Manager
 public class RobotNameManager {
   public final String name;
@@ -11,12 +7,18 @@ public class RobotNameManager {
 
   /** @param robotNames List out all the robot names this code is targeted for. The first name is considered the "competition robot"*/
   public RobotNameManager(String... robotNames) {
-    if (RobotState.isSimulation())
-      name = robotNames[0];
-    else name = Filer.readFile(Filer.HOME_PATH + "robot.txt");
-    id = Arrays.asList(robotNames).indexOf(name);
-    if (id == -1)
-      console.error("Invalid Robot Name!");
+    this.id = 0;
+    this.name = robotNames[id];
+//
+//
+//    if (RobotState.isSimulation()) {
+//      name = robotNames[0];
+//    }
+//    else name = Filer.readFile(Filer.HOME_PATH + "robot.txt");
+//    id = Arrays.asList(robotNames).indexOf(name);
+//    if (id == -1) {
+//      console.error("Invalid Robot Name!");
+//    }
   }
 
   /** Returns a value that corresponds to the robotName index */
