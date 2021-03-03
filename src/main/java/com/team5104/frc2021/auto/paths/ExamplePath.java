deca09075c76929bb2e7d35a941e1b5629449c02
Liam Snow <liamsnow03@gmail.com>
2021-03-02 20:01:17 -0500

    Update ExamplePath.java

--- src/main/java/com/team5104/frc2021/auto/paths/ExamplePath.java
+++ src/main/java/com/team5104/frc2021/auto/paths/ExamplePath.java
@@ -1,11 +1,10 @@
 /* BreakerBots Robotics Team (FRC 5104) 2020 */
 package com.team5104.frc2021.auto.paths;
 
+import com.team5104.frc2021.auto.actions.DriveTrajectory;
 import com.team5104.lib.auto.AutoPath;
 import com.team5104.lib.auto.Odometry;
 import com.team5104.lib.auto.Position;
-import com.team5104.frc2021.auto.actions.DriveTrajectory;
-import com.team5104.frc2021.auto.actions.PickupBall;
 
 public class ExamplePath extends AutoPath {
   public void start() {
@@ -14,21 +13,8 @@ public class ExamplePath extends AutoPath {
 
     run(new DriveTrajectory(false,
                             new Position(0, 0, 0),
-                            new Position(15, 15, 90)
+                            new Position(10, 10, 90)
     ));
-
-    if (run(new PickupBall(2000, false))) {
-      run(new DriveTrajectory(false,
-        new Position(15, 15, 90),
-        new Position(0.1, 30, 90)
-      ));
-    }
-    else {
-      run(new DriveTrajectory(false,
-        new Position(15, 15, 90),
-        new Position(30, 30, 90)
-      ));
-    }
   }
 }
 
